3ebef2b9594cf50a694e79fc8f54cb0b8bf73449
JLayseca <jlayseca@csumb.edu>
2023-03-14 15:17:27 -0700

    Space Invaders Update

--- Space Invaders V3/Assets/Scripts/SceneSwitch.cs
+++ Space Invaders V3/Assets/Scripts/SceneSwitch.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 public class SceneSwitch : MonoBehaviour
 {
     public string gameSceneName;
+
+    void Start()
+    {
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
     public void LoadGameScene()
     {
         SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
