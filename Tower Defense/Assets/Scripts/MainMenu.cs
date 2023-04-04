0c9dbaef48c102048432f0103f0a552d99558a60
JLayseca <jlayseca@csumb.edu>
2023-04-03 21:03:15 -0700

    Tower Defense 2 update

---
+++ Tower Defense/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public string levelToLoad = "MainLevel";
+	
+	//public SceneFader sceneFader;
+
+	public void Play()
+	{
+		//sceneFader.FadeTo(levelToLoad);
+        SceneManager.LoadScene(levelToLoad);
+	}
+
+	public void Quit()
+	{
+		Debug.Log("Bye!");
+		Application.Quit();
+	}
+
+}
